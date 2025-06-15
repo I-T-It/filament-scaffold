1f3f815865d1e3c3033e646325b0a52a1ebba64d
Robbin "Roboroads" Schepers <robbin@deschepers.nl>
2025-06-16 01:11:29 +0200

    feat: pint

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,21 +3,21 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-    protected static ?string $modelLabel = "Admin";
-    protected static ?string $pluralModelLabel = "Admins";
+
+    protected static ?string $modelLabel = 'Admin';
+
+    protected static ?string $pluralModelLabel = 'Admins';
+
     protected static ?string $navigationGroup = 'Administratie';
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
